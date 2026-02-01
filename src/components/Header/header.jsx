1125e745522cc8b4bf15822bf713847ab03d965d
maite <maite.sanchez@gm2dev.com>
2026-02-01 18:54:41 -0300

    feat(header): structure

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,19 @@
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        PS COPIERS
+      </div>
+
+      <nav className="header__nav">
+        <a href="#">Sobre nosotros</a>
+        <a href="#">Servicios</a>
+        <a href="#">Productos</a>
+        <a href="#">Contacto</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
